66c32a403ca78fd67a2af2841dc10bab7a5bd2af
Neutrovertido <russellcode.james@gmail.com>
2020-03-09 17:52:01 -0600

    Exercise_14

---
+++ Java-course-2020/src/app/Exercise_14.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.util.Scanner;
+import java.lang.Math;
+public class Exercise_14{
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        Integer sum = 0;
+        double rc;
+        for (int i=0; i<10; i++){
+            System.out.print("Ingrese el numero #" + (i+1) + ": ");
+            sum = sum + sc.nextInt();
+        }
+        System.out.println("La suma es de: " + sum);
+        System.out.println("El promedio es de: " + sum / 10);
+        if (sum <= 100){
+            for (int i=sum; i>0; i--){
+                System.out.print(i + " - ");
+            }
+            System.out.println();
+        } else {
+            System.out.printf("La raiz cuadrada es de: %2f", Math.sqrt(sum));
+        }
+    }
+
+}
