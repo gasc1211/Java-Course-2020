62ee006d0fdf27441179e924f57f79ab1ae7622e
Neutrovertido <russellcode.james@gmail.com>
2020-03-10 10:24:18 -0600

    Exercise 83 + 86

---
+++ Java-course-2020/src/app/Exercise_83.java
@@ -0,0 +1,17 @@
+package app;
+
+import java.util.Random;
+public class Exercise_83{
+
+    public static void main (String[] args){
+        Random ra = new Random();
+        int dado1, dado2;
+        int intentos = 0;
+        do{
+            dado1 = ra.nextInt(6-1+1)+1;
+            dado2 = ra.nextInt(6-1+1)+1;
+            intentos++;
+        }while(dado1+dado2!=11);
+        System.out.printf("Tomo %d intentos lograr el 11 en los dados!\n",intentos);
+    }
+}
