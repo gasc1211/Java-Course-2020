62deeba7e52f7d3c3664118ffc69e27504d8498e
Gabriel Suazo <48170476+Gasc1211@users.noreply.github.com>
2020-03-12 13:00:56 -0600

    Exercises 91 & 92

---
+++ Java-course-2020/src/app/Exercise_88.java
@@ -0,0 +1,9 @@
+package app;
+
+import javax.swing.JOptionPane;
+
+public class Exercise_88 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Java-course-2020/src/app/Exercise_92.java
@@ -0,0 +1,31 @@
+package app;
+
+import javax.swing.JOptionPane;
+
+public class Exercise_92 {
+    public static void main(String[] args) {
+
+        int n[] = new int[3], cache;
+
+        do {
+            n[0] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el número 1: "));
+            n[1] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el número 2: "));
+            n[2] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el número 3: "));
+
+            for (int i = 0; i < n.length; i++) {
+                for (int j = i+1; j < n.length; j++) {
+                    if (n[j] > n[i]) {
+                        cache = n[i];
+                        n[i] = n[j];
+                        n[j] = cache;
+                    }
+                }
+            }
+
+            JOptionPane.showMessageDialog(null, "El valor central es de: " + n[1], "Resultado", 3);
+
+        } while ( (n[0] + n[1] + n[2]) != 15);
+
+
+    }
+}
