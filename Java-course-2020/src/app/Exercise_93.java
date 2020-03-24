5e3e1da8a8293ad5071cec1b16878c3a9a398642
Neutrovertido <russellcode.james@gmail.com>
2020-03-24 17:29:56 -0600

    Added forgotten code

--- Java-course-2020/src/app/Exercise_93.java
+++ Java-course-2020/src/app/Exercise_93.java
@@ -5,8 +5,23 @@ public class Exercise_93 {
 
     public static void main (String[] args) {
 
-        
+        int billetes[] = {500, 100, 50, 20, 10, 5, 2, 1};
+        int bill[] = new int[8];
 
+        int cant = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad a retirar: "));
+        int cache = cant;
+
+        for (int i=0; i < 8; i++) {
+            bill[i] = (cache - (cache % billetes[i])) / billetes[i];
+            cache = cant % billetes[i];
+        }
+
+        String msg = "";
+        for (int i=0; i<8; i++) {
+            msg += "Cantidad de billetes de " + billetes[i] + ": " + bill[i] + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, msg);
     }
 
 }
