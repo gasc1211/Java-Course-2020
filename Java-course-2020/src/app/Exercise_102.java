369fe9802a035f70f6487fb2af45184e7173b604
Gasc1211 <48170476+Gasc1211@users.noreply.github.com>
2020-03-25 11:12:25 -0600

    Code optimization

--- Java-course-2020/src/app/Exercise_102.java
+++ Java-course-2020/src/app/Exercise_102.java
@@ -5,23 +5,20 @@ import javax.swing.JOptionPane;
 public class Exercise_102 {
     public static void main(String[] args) {
        
-        String q[] = {JOptionPane.showInputDialog(null, "¿Qué significa html?"), 
-        JOptionPane.showInputDialog(null, "¿Qué significa css?"), 
-        JOptionPane.showInputDialog(null, "¿Qué sistema es el actual estándar en redes?"), 
-        JOptionPane.showInputDialog(null, "¿Qué empresa inventó java?"), 
-        JOptionPane.showInputDialog(null, "¿Qué lenguaje de programación se usa en las páginas web?"), 
-        JOptionPane.showInputDialog(null, "¿Cuáles son los tipos de redes según el medio que usan?"),
-        JOptionPane.showInputDialog(null, "¿Qué significa PDU?"),
-        JOptionPane.showInputDialog(null, "¿Qué significa TCP?"),
-        JOptionPane.showInputDialog(null, "¿Qué significa IP?"),
-        JOptionPane.showInputDialog(null, "¿Qué significa MAC?")};
+        String q[] = {"¿Qué significa html?", "¿Qué significa css?", "¿Qué sistema es el actual estándar en redes?", "¿Qué empresa inventó java?", "¿Qué lenguaje de programación se usa en las páginas web?", "¿Cuáles son los tipos de redes según el medio que usan?", "¿Qué significa PDU?", "¿Qué significa TCP?", "¿Qué significa IP?", "¿Qué significa MAC?"};
 
         String a[] = {"hyper text markup language", "cascading style sheets", "TCP/IP", "Sun Microsystems", "Javascript", "inalámbricas y cableadas", "protocol data unit", "transmission control protocol", "internet protocol", "media access control"};
+
+        String ua[] = new String[10];
+
+        for (int i = 0; i < a.length; i++) {
+            ua[i] = JOptionPane.showInputDialog(null, q[i]);
+        }
 
         byte g = 0;
 
         for (int i = 0; i < a.length; i++) {
-            if( a[i].equals(q[i])) {
+            if( a[i].equals(ua[i])) {
                 g++;
             }
         }
