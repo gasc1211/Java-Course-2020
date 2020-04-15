eed9171f72f89286b5d93ec59bb3e01df46181b6
Neutrovertido <russellcode.james@gmail.com>
2020-04-15 15:03:16 -0600

    Exercise_112 added

--- Java-course-2020/src/app/Exercise_112.java
+++ Java-course-2020/src/app/Exercise_112.java
@@ -3,6 +3,33 @@ package app;
 public class Exercise_112 {
 
     public static void main(String[] args) {
-        
+        String cache = "";
+        String actual = "";
+        for (int i = 0; i <= 99999; i++) {
+            cache = Integer.toString(i);
+            for (int ii = 0; ii < cache.length(); ii++) {
+                if (cache.charAt(ii) == '3') {
+                    actual += 'E';
+                } else {
+                    actual += cache.charAt(ii);
+                }
+            }
+            switch (actual.length()) {
+                case 1:
+                    actual = "0000" + actual;
+                    break;
+                case 2:
+                    actual = "000" + actual;
+                    break;
+                case 3:
+                    actual = "00" + actual;
+                    break;
+                case 4:
+                    actual = "0" + actual;
+                    break;
+            }
+            System.out.println(actual);
+            actual = "";
+        }
     }
 }
