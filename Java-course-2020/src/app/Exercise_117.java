94fee998bf0aca9c170fe572f78b2543561026b1
James Cabrera <russellcode.james@gmail.com>
2020-04-22 00:42:04 -0600

    Bug Fixes + First Data Vessels Aligned

--- Java-course-2020/src/app/Exercise_117.java
+++ Java-course-2020/src/app/Exercise_117.java
@@ -7,15 +7,18 @@ import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Exercise_117 {
 
     public static Font myFont = new Font("Arial", Font.PLAIN, 16);
 
+    public static JTextField cel = new JTextField(5);
+
     public Exercise_117() {
         JFrame root = new JFrame();
         root.setTitle("Conversor a grados Fahrenheit");
-        root.setSize(800, 600);
+        root.setSize(500, 500);
         root.setResizable(false);
         
         JPanel frame = new JPanel(new GridBagLayout());
@@ -24,10 +27,21 @@ public class Exercise_117 {
         JLabel titulo = new JLabel();
         titulo.setFont(new Font("Arial", Font.BOLD, 18));
         titulo.setText("Conversor a Fahrenheit");
+        con.fill = GridBagConstraints.HORIZONTAL;
         con.gridx = 0;
         con.gridy = 0;
-        con.ipadx = 3;
+        con.gridwidth = 3;
         frame.add(titulo, con);
+
+        JLabel lb1 = new JLabel("Celsius: ");
+        lb1.setFont(myFont);
+        con.gridy = 1;
+        con.gridwidth = 1;
+        frame.add(lb1, con);
+
+        cel.setFont(myFont);
+        con.gridx = 1;
+        frame.add(cel, con);
 
         root.add(frame);
         root.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
