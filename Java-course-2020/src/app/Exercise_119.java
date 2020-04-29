f75962a55e4f2e3a68410bd6568c639f84b57315
Gasc1211 <48170476+Gasc1211@users.noreply.github.com>
2020-04-29 09:13:56 -0600

    Refactored exercises 119 - 120.

--- Java-course-2020/src/app/Exercise_119.java
+++ Java-course-2020/src/app/Exercise_119.java
@@ -3,111 +3,117 @@ package app;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.ButtonGroup;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Exercise_119 {
+public class Exercise_119 extends javax.swing.JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JLabel lbl_nums;
+    private JTextField txt_n1;
+    private JTextField txt_n2;
+    private JRadioButton rad_add;
+    private JRadioButton rad_sub;
+    private JButton btn_calc;
 
     public Exercise_119() {
+        initComponents();
+    }
+
+    private void initComponents() {
 
         // Frame initialization
 
-        JFrame frame = new JFrame();
-        frame.setTitle("¿Suma o Resta?");
-        frame.setSize(320, 250);
-        frame.setResizable(false);
+        this.setTitle("¿Suma o Resta?");
+        this.setSize(320, 250);
+        this.setResizable(false);
 
         // Panel & GridBag Layout setup
 
-        JPanel panel = new JPanel(new GridBagLayout());
+        Container panel = this.getContentPane();
+        panel.setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.insets = new Insets(5, 5, 5, 5);
+        constraints.fill = GridBagConstraints.CENTER;
+        constraints.anchor = GridBagConstraints.CENTER;
 
         // Labels
 
-        constraints.fill = GridBagConstraints.LINE_START;
-
-        JLabel lbl_nums = new JLabel("Números:");
+        lbl_nums = new JLabel("Números:");
         constraints.gridx = 0;
         constraints.gridy = 0;
         panel.add(lbl_nums, constraints);
 
         // Text Boxes
 
-        constraints.fill = GridBagConstraints.CENTER;
-        constraints.anchor = GridBagConstraints.CENTER;
-
-        JTextField txt_n1 = new JTextField(10);
+        txt_n1 = new JTextField(10);
         constraints.gridx = 0;
         constraints.gridy = 1;
         panel.add(txt_n1, constraints);
 
-        JTextField txt_n2 = new JTextField(10);
+        txt_n2 = new JTextField(10);
         constraints.gridx = 0;
         constraints.gridy = 2;
         panel.add(txt_n2, constraints);
 
         // Radiobuttons
 
-        constraints.fill = GridBagConstraints.LINE_START;
-        constraints.anchor = GridBagConstraints.LINE_START;
-
-        JRadioButton rbtn_add = new JRadioButton("Suma");
+        rad_add = new JRadioButton("Suma");
         constraints.gridx = 0;
         constraints.gridy = 3;
-        panel.add(rbtn_add, constraints);
+        panel.add(rad_add, constraints);
 
-        JRadioButton rbtn_subs = new JRadioButton("Resta");
+        rad_sub = new JRadioButton("Resta");
         constraints.gridx = 0;
         constraints.gridy = 4;
-        panel.add(rbtn_subs, constraints);
+        panel.add(rad_sub, constraints);
 
         ButtonGroup group = new ButtonGroup();
-        group.add(rbtn_add);
-        group.add(rbtn_subs);
+        group.add(rad_add);
+        group.add(rad_sub);
 
         // Button
 
-        JButton btn_calc = new JButton("Calcular");
+        btn_calc = new JButton("Calcular");
         btn_calc.addActionListener(new ActionListener() {
-            
             @Override
             public void actionPerformed(ActionEvent e) {
-                
-                Double n1 = Double.parseDouble(txt_n1.getText());
-                Double n2 = Double.parseDouble(txt_n2.getText());
-                
-                if (rbtn_add.isSelected()) {
-                    frame.setTitle(String.format("El resultado es: %.2f.", n1 + n2));
-                } else if (rbtn_subs.isSelected()) {
-                    frame.setTitle(String.format("El resultado es: %.2f.", n1 - n2));
-                }
-
+                btn_calcAction(e);
             }
-
         });
         constraints.gridx = 0;
         constraints.gridy = 5;
         constraints.gridwidth = 2;
         panel.add(btn_calc, constraints);
 
-        
-        // Frame 
+        // Frame final setup
 
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    private void btn_calcAction(ActionEvent e) {
+
+        Double n1 = Double.parseDouble(txt_n1.getText());
+        Double n2 = Double.parseDouble(txt_n2.getText());
+
+        if (rad_add.isSelected()) {
+            this.setTitle(String.format("El resultado es: %.2f.", n1 + n2));
+        } else if (rad_sub.isSelected()) {
+            this.setTitle(String.format("El resultado es: %.2f.", n1 - n2));
+        }
+
     }
 
     public static void main(String[] args) {
-     new Exercise_119();   
+        new Exercise_119();
     }
 }
--- Java-course-2020/src/app/Exercise_120.java
+++ Java-course-2020/src/app/Exercise_120.java
@@ -10,24 +10,45 @@ import javax.swing.JTextField;
 
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Exercise_120 {
+public class Exercise_120 extends javax.swing.JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JLabel lbl_n1;
+    private JLabel lbl_n2;
+    private JTextField txt_n1;
+    private JTextField txt_n2;
+    private JTextField txt_add;
+    private JTextField txt_sub;
+    private JTextField txt_mul;
+    private JTextField txt_div;
+    private JCheckBox chk_add;
+    private JCheckBox chk_sub;
+    private JCheckBox chk_mul;
+    private JCheckBox chk_div;
+    private JButton btn_exit;
+    private JButton btn_clear;
 
     public Exercise_120() {
+        initComponents();
+    }
 
-        // Frame initialization
+    public void initComponents() {
 
-        JFrame frame = new JFrame();
-        frame.setTitle("Operaciones Matemáticas");
-        frame.setSize(400, 320);
-        frame.setResizable(false);
+        // Frame initial setup
+
+        this.setTitle("Operaciones Matemáticas");
+        this.setSize(400, 320);
+        this.setResizable(false);
 
         // Panel & GridBag Layout setup
 
-        JPanel panel = new JPanel(new GridBagLayout());
+        Container panel = this.getContentPane();
+        panel.setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.insets = new Insets(5, 5, 5, 5);
 
@@ -35,27 +56,27 @@ public class Exercise_120 {
 
         constraints.fill = GridBagConstraints.LINE_START;
 
-        JLabel lbl_n1 = new JLabel("Número 1:");
+        lbl_n1 = new JLabel("Número 1:");
         constraints.gridx = 0;
         constraints.gridy = 0;
         panel.add(lbl_n1, constraints);
 
-        JLabel lbl_n2 = new JLabel("Número 2:");
+        lbl_n2 = new JLabel("Número 2:");
         constraints.gridx = 1;
         constraints.gridy = 0;
         panel.add(lbl_n2, constraints);
 
         // Text Boxes
-        
+
         constraints.fill = GridBagConstraints.CENTER;
         constraints.anchor = GridBagConstraints.CENTER;
 
-        JTextField txt_n1 = new JTextField(10);
+        txt_n1 = new JTextField(10);
         constraints.gridx = 0;
         constraints.gridy = 1;
         panel.add(txt_n1, constraints);
 
-        JTextField txt_n2 = new JTextField(10);
+        txt_n2 = new JTextField(10);
         constraints.gridx = 1;
         constraints.gridy = 1;
         panel.add(txt_n2, constraints);
@@ -70,25 +91,25 @@ public class Exercise_120 {
         cons_txt.fill = GridBagConstraints.LINE_START;
         cons_txt.anchor = GridBagConstraints.LINE_START;
 
-        JTextField txt_add = new JTextField(18);
+        txt_add = new JTextField(15);
         txt_add.setEnabled(false);
         cons_txt.gridx = 0;
         cons_txt.gridy = 0;
         internalPanel_txt.add(txt_add, cons_txt);
 
-        JTextField txt_sub = new JTextField(18);
+        txt_sub = new JTextField(15);
         txt_sub.setEnabled(false);
         cons_txt.gridx = 0;
         cons_txt.gridy = 1;
         internalPanel_txt.add(txt_sub, cons_txt);
 
-        JTextField txt_mul = new JTextField(18);
+        txt_mul = new JTextField(15);
         txt_mul.setEnabled(false);
         cons_txt.gridx = 0;
         cons_txt.gridy = 2;
         internalPanel_txt.add(txt_mul, cons_txt);
 
-        JTextField txt_div = new JTextField(18);
+        txt_div = new JTextField(15);
         txt_div.setEnabled(false);
         cons_txt.gridx = 0;
         cons_txt.gridy = 3;
@@ -108,68 +129,44 @@ public class Exercise_120 {
         cons_chk.fill = GridBagConstraints.LINE_START;
         cons_chk.anchor = GridBagConstraints.LINE_START;
 
-        JCheckBox chk_add = new JCheckBox("Suma");
+        chk_add = new JCheckBox("Suma");
         chk_add.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (chk_add.isSelected()) {
-                    Double n1 = Double.parseDouble(txt_n1.getText());
-                    Double n2 = Double.parseDouble(txt_n2.getText());
-                    txt_add.setText(String.format("= %.2f", n1 + n2));
-                } else {
-                    txt_add.setText("Operación no seleccionada");
-                }
+                operate(e, chk_add, txt_add, '+');
             }
         });
         cons_chk.gridx = 0;
         cons_chk.gridy = 0;
         internalPanel_chk.add(chk_add, cons_chk);
 
-        JCheckBox chk_sub = new JCheckBox("Resta");
+        chk_sub = new JCheckBox("Resta");
         chk_sub.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (chk_sub.isSelected()) {
-                    Double n1 = Double.parseDouble(txt_n1.getText());
-                    Double n2 = Double.parseDouble(txt_n2.getText());
-                    txt_sub.setText(String.format("= %.2f", n1 - n2));
-                } else {
-                    txt_sub.setText("Operación no seleccionada");
-                }
+                operate(e, chk_sub, txt_sub, '-');
             }
         });
         cons_chk.gridx = 0;
         cons_chk.gridy = 1;
         internalPanel_chk.add(chk_sub, cons_chk);
 
-        JCheckBox chk_mul = new JCheckBox("Multiplicación");
+        chk_mul = new JCheckBox("Multiplicación");
         chk_mul.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (chk_mul.isSelected()) {
-                    Double n1 = Double.parseDouble(txt_n1.getText());
-                    Double n2 = Double.parseDouble(txt_n2.getText());
-                    txt_mul.setText(String.format("= %.2f", n1 * n2));
-                } else {
-                    txt_mul.setText("Operación no seleccionada");
-                }
+                operate(e, chk_mul, txt_mul, '*');
             }
         });
         cons_chk.gridx = 0;
         cons_chk.gridy = 2;
         internalPanel_chk.add(chk_mul, cons_chk);
 
-        JCheckBox chk_div = new JCheckBox("División");
+        chk_div = new JCheckBox("División");
         chk_div.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (chk_div.isSelected()) {
-                    Double n1 = Double.parseDouble(txt_n1.getText());
-                    Double n2 = Double.parseDouble(txt_n2.getText());
-                    txt_div.setText(String.format("= %.2f", n1 / n2));
-                } else {
-                    txt_div.setText("Operación no seleccionada");
-                }
+                operate(e, chk_div, txt_div, '/');
             }
         });
         cons_chk.gridx = 0;
@@ -185,37 +182,25 @@ public class Exercise_120 {
         constraints.fill = GridBagConstraints.CENTER;
         constraints.anchor = GridBagConstraints.CENTER;
 
-        JButton btn_calc = new JButton("Salir");
-        btn_calc.addActionListener(new ActionListener() {
+        btn_exit = new JButton("Salir");
+        btn_exit.addActionListener(new ActionListener() {
 
             @Override
-            public void actionPerformed(ActionEvent e) {                
-                System.exit(0);
+            public void actionPerformed(ActionEvent e) {
+                btn_exitAction(e);
             }
 
         });
         constraints.gridx = 1;
         constraints.gridy = 3;
-        panel.add(btn_calc, constraints);
+        panel.add(btn_exit, constraints);
 
-        JButton btn_clear = new JButton("Limpiar");
+        btn_clear = new JButton("Limpiar");
         btn_clear.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                txt_n1.setText("");
-                txt_n2.setText("");
-                txt_add.setText("");
-                txt_sub.setText("");
-                txt_mul.setText("");
-                txt_div.setText("");
-                
-                chk_add.setSelected(false);
-                chk_sub.setSelected(false);
-                chk_mul.setSelected(false);
-                chk_div.setSelected(false);
-
+                btn_clearAction(e);
             }
 
         });
@@ -223,15 +208,55 @@ public class Exercise_120 {
         constraints.gridy = 3;
         panel.add(btn_clear, constraints);
 
-        // Frame
+        // Frame final setup
 
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    private void operate(ActionEvent e, JCheckBox chk, JTextField txt, char operation) {
+
+        Double n1 = Double.parseDouble(txt_n1.getText());
+        Double n2 = Double.parseDouble(txt_n2.getText());
+
+        if (chk.isSelected()) {
+            switch (operation) {
+                case '+':
+                    txt.setText(String.format(" = %.2f", n1 + n2));
+                    break;
+                case '-':
+                    txt.setText(String.format(" = %.2f", n1 - n2));
+                    break;
+                case '*':
+                    txt.setText(String.format(" = %.2f", n1 * n2));
+                    break;
+                case '/':
+                    txt.setText(String.format(" = %.2f", n1 / n2));
+                    break;
+            }
+        } else {
+            txt.setText("Operación no seleccionada.");
+        }
+    }
+
+    private void btn_exitAction(ActionEvent e) {
+        System.exit(0);
+    }
+
+    private void btn_clearAction(ActionEvent e) {
+        txt_n1.setText("");
+        txt_n2.setText("");
+        txt_add.setText("");
+        txt_sub.setText("");
+        txt_mul.setText("");
+        txt_div.setText("");
+        chk_add.setSelected(false);
+        chk_sub.setSelected(false);
+        chk_mul.setSelected(false);
+        chk_div.setSelected(false);
     }
 
     public static void main(String[] args) {
         new Exercise_120();
     }
-
 }
