ac6df0e0172b72a6c47c529db2bc2b7815bb3cdc
James Cabrera <61665012+Neutrovertido@users.noreply.github.com>
2020-05-05 05:55:06 -0600

    Exercises 122-123 (#38)
    * Initial Setup
    
    * Layout Done
    
    * Added UUID
    
    * Made it work
    
    * Prettier code
    
    * Initial Setup + Added Layout
    
    * Made it work
    
    * Prettier code

---
+++ Java-course-2020/src/app/Exercise_122.java
@@ -0,0 +1,112 @@
+package app;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Exercise_122 extends JFrame {
+
+    private static final long serialVersionUID = 6825655101757518603L;
+
+    private Font myFont = new Font("Arial", Font.PLAIN, 16);
+    private Font tFont = new Font("Arial", Font.BOLD, 18);
+
+    private JLabel titulo = new JLabel();
+    private JComboBox cpais = new JComboBox();
+    private JTextField ccapital = new JTextField(12);
+
+    private String paises[] = { "Alemania", "Italia", "Grecia", "Suecia", "Rusia", "Noruega", "Francia", "Polonia",
+            "España", "Inglaterra" };
+
+    public Exercise_122() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        this.setResizable(false);
+        this.setSize(400, 150);
+        this.setTitle("Europa");
+
+        JPanel root = new JPanel();
+        root.setLayout(new GridBagLayout());
+        GridBagConstraints con = new GridBagConstraints();
+        con.insets = new Insets(5, 5, 5, 5);
+        con.anchor = GridBagConstraints.CENTER;
+        this.add(root);
+
+        titulo.setText("Capital del país");
+        titulo.setFont(tFont);
+        con.gridx = 0;
+        con.gridy = 0;
+        con.gridwidth = 2;
+        root.add(titulo, con);
+
+        for (String i : paises) {
+            cpais.addItem(i);
+        }
+
+        cpais.setFont(myFont);
+        con.gridx = 0;
+        con.gridy = 1;
+        con.gridwidth = 1;
+        con.anchor = GridBagConstraints.LINE_START;
+        cpais.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String pais = String.valueOf(cpais.getSelectedItem());
+                String x = "";
+                switch (pais) {
+                    case "Alemania":
+                        x = "Berlín";
+                        break;
+                    case "Italia":
+                        x = "Roma";
+                        break;
+                    case "Grecia":
+                        x = "Atenas";
+                        break;
+                    case "Suecia":
+                        x = "Estocolmo";
+                        break;
+                    case "Rusia":
+                        x = "Moscú";
+                        break;
+                    case "Noruega":
+                        x = "Oslo";
+                        break;
+                    case "Francia":
+                        x = "París";
+                        break;
+                    case "Polonia":
+                        x = "Varsovia";
+                        break;
+                    case "España":
+                        x = "Madrid";
+                        break;
+                    case "Inglaterra":
+                        x = "Londres";
+                        break;
+                }
+                ccapital.setText(x);
+            }
+        });
+        root.add(cpais, con);
+
+        ccapital.setEnabled(false);
+        ccapital.setFont(myFont);
+        ccapital.setText("Berlín");
+        con.gridx = 1;
+        con.gridy = 1;
+        root.add(ccapital, con);
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Exercise_122();
+    }
+
+}
---
+++ Java-course-2020/src/app/Exercise_123.java
@@ -0,0 +1,96 @@
+package app;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Exercise_123 extends JFrame {
+
+    private static final long serialVersionUID = -3094403425238379186L;
+
+    private Font myFont = new Font("Arial", Font.PLAIN, 16);
+    private Font tFont = new Font("Arial", Font.BOLD, 18);
+
+    private JLabel titulo = new JLabel();
+    private JComboBox cmes = new JComboBox();
+
+    private String meses[] = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+            "Octubre", "Noviembre", "Diciembre" };
+    private JFrame refval = this;
+
+    public Exercise_123() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        this.setResizable(false);
+        this.setSize(400, 150);
+        this.setTitle("Invierno");
+
+        JPanel root = new JPanel();
+        root.setLayout(new GridBagLayout());
+        GridBagConstraints con = new GridBagConstraints();
+        con.insets = new Insets(5, 5, 5, 5);
+        con.anchor = GridBagConstraints.CENTER;
+        this.add(root);
+
+        titulo.setText("Elija un mes");
+        titulo.setFont(tFont);
+        con.gridx = 0;
+        con.gridy = 0;
+        root.add(titulo, con);
+
+        for (String i : meses) {
+            cmes.addItem(i);
+        }
+
+        cmes.setFont(myFont);
+        con.gridx = 0;
+        con.gridy = 1;
+        con.anchor = GridBagConstraints.LINE_START;
+        cmes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String mes = String.valueOf(cmes.getSelectedItem());
+                String y = "";
+                switch (mes) {
+                    case "Diciembre":
+                    case "Enero":
+                    case "Febrero":
+                        y = "Invierno";
+                        break;
+                    case "Marzo":
+                    case "Abril":
+                    case "Mayo":
+                    case "Junio":
+                        y = "Primavera";
+                        break;
+                    case "Julio":
+                    case "Agosto":
+                    case "Septiembre":
+                        y = "Verano";
+                        break;
+                    case "Octubre":
+                    case "Noviembre":
+                        y = "Otoño";
+                        break;
+                    default:
+                        y = "Estacion";
+                        break;
+                }
+                refval.setTitle(y);
+            }
+        });
+        root.add(cmes, con);
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Exercise_123();
+    }
+
+}
