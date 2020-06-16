cc5073fd7de83550ab104725ac5250bc53a1a96d
Neutrovertido <russellcode.james@gmail.com>
2020-06-15 21:23:30 -0600

    Exercise_130 finished

--- Java-course-2020/src/app/Exercise_130.java
+++ Java-course-2020/src/app/Exercise_130.java
@@ -5,16 +5,34 @@ import java.awt.event.*;
 
 public class Exercise_130 extends JFrame{
 
-    private JList list = new JList();
+    private JList list;
+    private String colores[] = {"Rojo", "Verde","Azul"};
+    private JButton aceptar = new JButton("Aceptar");
+    private JTextField resultado = new JTextField(25);
 
     public Exercise_130() {
-        this.setSize(400, 400);
+        this.setSize(400, 150);
         this.setResizable(false);
 
         JPanel root = new JPanel();
         this.add(root);
 
-        
+        list = new JList(colores);
+        list.setSelectedIndex(0);
+        list.setPreferredSize(new Dimension(150, 50));
+        root.add(list);
+
+        aceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultado.setText("El color seleccionado es: " + list.getSelectedValue());
+            }
+        });
+        root.add(aceptar);
+
+        resultado.setText("El color seleccionado es: ");
+        resultado.setEnabled(false);
+        root.add(resultado);
 
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
