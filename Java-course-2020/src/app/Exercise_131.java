5b0b8d34b178abef46b9f987402da021c9d856e4
Neutrovertido <russellcode.james@gmail.com>
2020-06-15 23:55:17 -0600

    Progress 2

---
+++ Java-course-2020/src/app/Exercise_131.java
@@ -0,0 +1,19 @@
+package app;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Exercise_131 extends JFrame{
+    public Exercise_131() {
+        this.setSize(400, 400);
+        this.setResizable(false);
+
+
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Exercise_131();
+    }
+}
